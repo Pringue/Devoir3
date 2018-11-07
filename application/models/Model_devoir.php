7997c7b973ed257eb4a5ad793a4513eabe81b834
Jerome Pringue <jeromep94@hotmail.fr>
2018-11-07 19:51:47 +0100

    Base devoir

---
+++ application/models/Model_devoir.php
@@ -0,0 +1,6 @@
+<?php
+class Model_devoir extends CI_Controller
+{
+    
+}
+?>
---
+++ application/views/welcome_message.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Page Title</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<script src="assets/JQuery/jquery-3.1.1.js"></script>
+	<script src="assets/JS/mesFonctions.js"></script>
+	<script>
+	$(
+		function
+		{
+			$("#").(function{
+
+			});
+		}
+	)
+	</script>dsd
+</head>
+<body>
+	<h1></h1>
+</body>
+</html>
